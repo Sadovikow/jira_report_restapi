da23a8a4744c25baafa3aff75ff6d577873e0682
Sergey Sadovikow <nexxerr@gmail.com>
2021-12-23 16:23:14 +0300

    Init

---
+++ functions.php
@@ -0,0 +1,131 @@
+<?php
+
+//define("JIRA_URL", "https://jira");
+//define("USERNAME", "login");
+//define("PASSWORD", "token");
+
+/**
+ *
+	# /rest/api/2/filter/xxxxxx
+	# /rest/api/latest/project - получить список проектов
+	# /rest/api/latest/project/WEB - получить информацию о проекте с ключом WEB
+	# /rest/api/latest/issue/WEB-13 - получить информацию о задаче WEB-13
+	# /rest/api/latest/priority - список возможных приоритетов для задач
+	# /rest/api/latest/issue/WEB-13/worklog - список ворклогов для задачи с ключом WEB-13
+*/
+
+function getJiraFilter($resource) {
+	/*
+	 *
+	 */
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => JIRA_URL . '/rest/api/2/filter/' . $resource,
+		CURLOPT_USERPWD => USERNAME . ':' . PASSWORD,
+		CURLOPT_HTTPHEADER => array('Content-type: application/json', 'Authorization: Bearer NDI2NjYwNjMyOTE3Oly7w/KEBRRpomI0EjoK1ijke57I'),
+		CURLOPT_RETURNTRANSFER => true
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	$result = json_decode($result, true);
+	return $result;
+
+}
+
+function getFilterLink($filterData) {
+	/**
+	* Функция выделяет из входящего массива фильтра Jira JQL ссылку на массив
+	*/
+	$link = $filterData['searchUrl'];
+	return $link;
+}
+
+function getSprintTasks($link) {
+	/**
+	* Запрос к таскам спринта по JQL ссылке
+	*/
+	$ch = curl_init();
+	//configure CURL
+	curl_setopt_array($ch, array(
+		CURLOPT_URL => $link,
+		CURLOPT_USERPWD => USERNAME . ':' . PASSWORD,
+		CURLOPT_HTTPHEADER => array('Content-type: application/json', 'Authorization: Bearer NDI2NjYwNjMyOTE3Oly7w/KEBRRpomI0EjoK1ijke57I'),
+		CURLOPT_RETURNTRANSFER => true
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	$result = json_decode($result, true);
+	return $result;
+}
+
+function getJiraResultTasks($tasks, $show = false, $debug = false) {
+	/**
+	* Перебор задач из массива
+	* $show - отображает пример массива
+	*/
+	$arTasks = $tasks['issues'];
+	if($show) {
+		foreach ($arTasks as $key => $task) {
+			echo $task['key'].' | '.$task['fields']['status']['name'].' | '.$task['fields']['summary'].' | Время: '.$task['fields']['aggregatetimeestimate']/60/60 . '<br>';
+		}
+	}
+	if($debug) {
+		echo '<pre>';
+		print_r($arTasks);
+		echo '</pre>';
+	}
+	return $arTasks;
+}
+
+function makeArrayByWorkers($tasks) {
+	/**
+	* Отсортированный массив по сотрудникам
+	*/
+	$arTasks = $tasks['issues'];
+	$arWorkers = Array();
+	foreach ($arTasks as $key => $task) {
+
+		if(!isset($arWorkers[$task['fields']['assignee']['name']])) {
+			$arWorkers[$task['fields']['assignee']['name']]['name'] = $task['fields']['assignee']['name'];
+			$arWorkers[$task['fields']['assignee']['name']]['displayName'] = $task['fields']['assignee']['displayName'];
+			$arWorkers[$task['fields']['assignee']['name']]['sum_hours'] = 0;
+			$arWorkers[$task['fields']['assignee']['name']]['sum_minutes'] = 0;
+			$arWorkers[$task['fields']['assignee']['name']]['tasks_count'] = 0;
+
+		}
+		$arWorkers[$task['fields']['assignee']['name']]['sum_hours'] = $arWorkers[$task['fields']['assignee']['name']]['sum_hours'] + $task['fields']['aggregatetimeestimate']/60/60;
+		$arWorkers[$task['fields']['assignee']['name']]['sum_minutes'] += $task['fields']['aggregatetimeestimate']/60;
+		$arWorkers[$task['fields']['assignee']['name']]['tasks_count'] = $arWorkers[$task['fields']['assignee']['name']]['tasks_count'] + 1;
+
+		$arWorkers[$task['fields']['assignee']['name']]['tasks'][$task['key']] = $task['fields'];
+
+	}
+	return $arWorkers;
+}
+
+function makeArrayByComponents($tasks) {
+	/**
+	* Отсортированный массив по компонентам
+	*/
+	$arTasks = $tasks['issues'];
+	$arComponents = Array();
+	foreach ($arTasks as $key => $task) {
+
+		if(!isset($arComponents[$task['fields']['components'][0]['name']])) {
+			$arComponents[$task['fields']['components'][0]['name']]['name'] = $task['fields']['components'][0]['name'];
+			$arComponents[$task['fields']['components'][0]['name']]['sum_hours'] = 0;
+			$arComponents[$task['fields']['components'][0]['name']]['sum_minutes'] = 0;
+			$arComponents[$task['fields']['components'][0]['name']]['tasks_count'] = 0;
+
+		}
+		$arComponents[$task['fields']['components'][0]['name']]['sum_hours'] = $arComponents[$task['fields']['components'][0]['name']]['sum_hours'] + $task['fields']['aggregatetimeestimate']/60/60;
+		$arComponents[$task['fields']['components'][0]['name']]['sum_minutes'] += $task['fields']['aggregatetimeestimate']/60;
+		$arComponents[$task['fields']['components'][0]['name']]['tasks_count'] = $arComponents[$task['fields']['components'][0]['name']]['tasks_count'] + 1;
+
+		$arComponents[$task['fields']['components'][0]['name']]['tasks'][$task['key']] = $task['fields'];
+
+	}
+	return $arComponents;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,134 @@
+<?php
+if (file_exists("functions.php")) {
+    require_once("functions.php");
+}
+
+if (file_exists("access.php")) {
+    require_once("access.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sprint Reporter</title>
+	<link rel="stylesheet" href="styles.css?3">
+</head>
+<body>
+		<div class="wrapper">
+		<header>
+			<div class="header__logo">
+				<div class="logo">
+					<a href="/" class="logo"></a>
+				</div>
+			</div>
+		</header>
+		<main>
+			<div class="container">
+				<h1>Sprint Report from Jira</h1>
+				<?php
+					/* Формирование данных из спринта */
+						$arFilterData = getJiraFilter('10501');
+						$linkSprint = getFilterLink($arFilterData);
+						$arSprintInfo = getSprintTasks($linkSprint);
+						//getJiraResultTasks($arSprintInfo, false, true); // debug
+					/* Формирование данных из спринта */
+
+					$arSprintTasks = getJiraResultTasks($arSprintInfo, 0, 0);
+					?>
+                    <div class="jreport">
+                        <div class="jreport__block clearfix">
+                        <table id="jira-employers" class="table table--left" style="height: auto;">
+                            <thead>
+                                <th>Сотрудник</th>
+                                <th>Количество задач</th>
+                                <th>Суммарное время (ч)</th>
+                                <th>Суммарное время (мин)</th>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $arWorkersTasks = makeArrayByWorkers($arSprintInfo);
+                            foreach ($arWorkersTasks as $key => $worker):?>
+                                <tr>
+                                    <td><?=$worker['name']?></td>
+                                    <td><?=$worker['tasks_count']?></td>
+                                    <td <?php if($worker['sum_hours'] > 28):?>class="table--red_cell"<?php endif;?>>
+                                        <?=$worker['sum_hours']?>
+
+                                    </td>
+                                    <td><?=$worker['sum_minutes']?></td>
+                                </tr>
+
+                            <?php
+                            endforeach;
+                                ?>
+                            </tbody>
+                        </table>
+
+                        <table id="jira-components" class="table table--right" style="height: auto;">
+                            <thead>
+                                <th>Компонент</th>
+                                <th>Количество задач</th>
+                                <th>Суммарное время (ч)</th>
+                            </thead>
+                            <tbody>
+                            <?php
+                            $arComponentsTasks = makeArrayByComponents($arSprintInfo);
+                            foreach ($arComponentsTasks as $key => $componentTask):?>
+                                <tr>
+                                    <td><?=$componentTask['name']?></td>
+                                    <td><?=$componentTask['tasks_count']?></td>
+                                    <td <?php if($componentTask['sum_hours'] > 28):?>class="table--red_cell"<?php endif;?>>
+                                        <?=$componentTask['sum_hours']?>
+                                    </td>
+                                </tr>
+
+                            <?php
+                            endforeach;
+                                ?>
+                            </tbody>
+                        </table>
+                        </div> <!--jreport__block-->
+                        <div class="jreport__block">
+                            <table id="jira-main" class="table" style="height: auto;">
+                                <thead>
+                                    <th>№</th>
+                                    <th>Название</th>
+                                    <th>Компонент</th>
+                                    <th>Статус</th>
+                                    <th>Ответственный</th>
+                                    <th>Время (ч)</th>
+                                    <th>Автор</th>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    foreach ($arSprintTasks as $key => $task):
+                                    ?>
+                                    <tr>
+                                        <td><a href="http://jira:8080/browse/WEB-886" target="_blank"><?=$task['key']?></a></td>
+                                        <td><?=$task['fields']['summary']?></td>
+                                        <td><?=$task['fields']['components'][0]['name']?></td>
+                                        <td><?=$task['fields']['status']['name']?></td>
+                                        <td><?=$task['fields']['assignee']['name']?></td>
+                                        <td><?=$task['fields']['aggregatetimeestimate']/60/60?></td>
+                                        <td><?=$task['fields']['creator']['name']?></td>
+                                    </tr>
+                                    <?php
+                                    endforeach;
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div> <!--jreport__block-->
+                    </div>
+			</div>
+		</main>
+	</div> <!-- wrapper -->
+	<div class="foot"></div>
+	<footer>
+
+	</footer>
+
+
+</body>
+</html>
